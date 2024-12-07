cbb00a4028e1779b32047ad2852da041061cb73c
LeatherPotato <thesussywussy06@gmail.com>
2024-12-07 18:51:10 +0000

    wrote all parser classes boilerplate and some of the more simple methods

--- src/org/uob/a2/parser/Tokeniser.java
+++ src/org/uob/a2/parser/Tokeniser.java
@@ -1,5 +1,6 @@
 package org.uob.a2.parser;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
@@ -11,6 +12,45 @@ import java.util.ArrayList;
  * </p>
  */
 public class Tokeniser {
+    ArrayList<Token> tokens;
 
+    public Tokeniser() {
+
+    }
+
+    public ArrayList<Token> getTokens() {
+        return tokens;
+    }
+
+    public String sanitise(String s) {
+        StringBuilder sanitised = new StringBuilder(new String());
+        char prevC = (char) 0;
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if (c != ' ') {
+                // if char is not equal to whitespace then it adds the lowercase character
+                sanitised.append(Character.toLowerCase(c));
+            }
+            else if (c != prevC) {
+                // if char is whitespace and is not equal to the previous, character, then it is added
+                // therefore only 1 whitespace character is added
+                sanitised.append(' ');
+            }
+            prevC = c;
+        }
+
+        // if whitespace character is at end of input string then it is removed
+        if (sanitised.charAt(sanitised.length()-1) == ' ') {
+            sanitised.setLength(sanitised.length()-1);
+        }
+        return sanitised.toString();
+    }
+
+    public void tokenise(String s) {
+        ArrayList<Token> tokens = new ArrayList<>();
+        String[] split = this.sanitise(s).split("\\s+");
+
+        // do stuff on the split array to turn it into token array then set this.tokens to local tokens variable
+    }
    
 }
